f32d65592e208327425ad7b52379fa9b22c72f4c
BysaniLahari <120727130+BysaniLahari@users.noreply.github.com>
2025-05-11 19:15:03 +0530

    Update dataclean.py
    changed to repartitition instead of coalesce

--- dataclean.py
+++ dataclean.py
@@ -1,23 +1,26 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, lower, trim, to_timestamp
 
-spark = SparkSession.builder.appName("DataCleaning").enableHiveSupport().getOrCreate()
+spark = SparkSession.builder \
+    .appName("DataClean") \
+    .enableHiveSupport() \
+    .getOrCreate()
 
-df = spark.read.option("header", True).option("inferSchema", True).csv("gs://online-payments-fraud/source/synthetic_fraud_dataset.csv")
+df = spark.read.csv("gs://online-payments-fraud/source/synthetic_fraud_dataset.csv", header=True, inferSchema=True)
 
 df = df.withColumnRenamed("timestamp", "time_stamp")
+
 df = df.toDF(*[c.lower() for c in df.columns])
+
 df = df.dropDuplicates().dropna()
+
+df = df.repartition(1)
 
 string_cols = ['card_type', 'transaction_type', 'merchant_category', 'device_type', 'location', 'authentication_method']
 for c in string_cols:
     df = df.withColumn(c, lower(trim(col(c))))
 
-
-df = df.withColumn("time_stamp", to_timestamp("time_stamp"))
-
 for col_name in ['account_balance', 'transaction_amount', 'risk_score']:
     df = df.filter(col(col_name) >= 0)
 
-
-df.coalesce(1).write.mode("overwrite").option("header", True).csv("gs://online-payments-fraud/destination/cleaned_data")
+df.write.mode("overwrite").csv("gs://online-payments-fraud/destination/cleaned_data", header=True)
